70d767ef9c8954a6d4b45135c837aa75b64f61b1
Sfinford <96948227+Sfinford@users.noreply.github.com>
2023-02-26 12:42:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const floatingBtn = document.getElementById("floating-btn");
 
+floatingBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  window.location.href = "https://sfinford.github.io/GPT-Prompts/";
+});
